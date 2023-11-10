446a854064194f77ed5b8c5ab490edff35aa087f
Carlos Requejo <carlos@cartech.com.ar>
2023-11-10 18:07:28 -0300

    Con Full Graphic Smart Controller sin terminar

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
+#endif
 
 #define OLED          // Con Display OLED
 
@@ -8,7 +14,9 @@
 // OLED 0.96"
 // U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 // OLED 1.3"
-U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+// U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+// Full Graphic Smart Controller
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ A5, /* data=*/ A4, /* CS=*/ 10, /* reset=*/ U8X8_PIN_NONE);
 #endif
 
 // Define the pin for the frequency measurement
@@ -33,8 +41,8 @@ int DefSpeed = 0;
 int RealSpeed = 0;
 int DefDelay = 1500;
 int DefAngle = 90;
-int Steps = 0;
-
+int DefSpeedPrev = 0;
+unsigned long Init_Time = 0;
 
 // put function declarations here:
 void setWaveforms(unsigned long, int);
@@ -43,6 +51,7 @@ void breakMotor(int speed, int delayTime);
 void countPulse(void);
 void configTimerForPulse(void);
 void configTimerForMeasure(void);
+void PrintStatus(String status);
 
 // prescaler of 1 will get us 8MHz - 488Hz
 // User a higher prescaler for lower freqncies
@@ -63,17 +72,22 @@ void setup() {
   pinMode(13, OUTPUT);
 
   #ifdef OLED
-  u8x8.begin();  // initialize with the I2C
-  u8x8.setPowerSave(0);
+  u8g2.begin();  // initialize with the I2C
+  u8g2.setPowerSave(0);
   // init done
-  u8x8.setFont(u8x8_font_pxplustandynewtv_r);  // u8x8_font_torussansbold8_r, u8x8_font_chroma48medium8_r, u8x8_font_victoriamedium8_r, u8x8_font_pxplustandynewtv_r
-  u8x8.draw1x2String(0,0,"Pimax S.R.L.");
-  u8x8.setCursor(0,2);             // Column, Row
-  u8x8.print("Nano Starter");
-  u8x8.setCursor(0,3);             // Column, Row
-  u8x8.print("Version 1.Git");     // SOFTWARE VERSION ---------------------------<<<<<<<<<<<<<<<<
+  // Set a small font
+  u8g2.setFont(u8g2_font_6x10_tf);
+  u8g2.setFontDirection(0);
+  u8g2.clearBuffer();
+  u8g2.setCursor(0, 10);
+  u8g2.print("Pimax S.R.L.");
+  u8g2.setCursor(0,20);             // Column, Row
+  u8g2.print("Nano Starter");
+  u8g2.setCursor(0,30);             // Column, Row
+  u8g2.print("Version 1.Git");     // SOFTWARE VERSION ---------------------------<<<<<<<<<<<<<<<<
+  u8g2.sendBuffer();
   delay(2000);
-  u8x8.clearDisplay();
+  u8g2.clearDisplay();
   #endif
 
   // Save TCC mode
@@ -86,89 +100,66 @@ void setup() {
   digitalWrite(11,LOW);
   NoPaso = true;
 
-  // Write a menu on display
-
-  #ifdef OLED
-  u8x8.setCursor(0,0);             // Column, Row
-  u8x8.print("Speed : ");
-  u8x8.setCursor(0,1);             // Column, Row
-  u8x8.print("Angle : ");
-  u8x8.setCursor(0,2);             // Column, Row
-  u8x8.print("Delay : ");
-  u8x8.setCursor(0,3);             // Column, Row
-  u8x8.print("Status: ");
-  u8x8.setCursor(0,4);             // Column, Row
-  u8x8.print("Real F: ");
-  #endif
-
   // Attach the interrupt
   attachInterrupt(digitalPinToInterrupt(INPUT_PIN), countPulse, RISING);
 }
 
 void loop() {
+  if (Init_Time == 0) Init_Time = millis();
+  // Read the buttons
   if (!digitalRead(A3)) {   // High Speed
     DefSpeed = 200; 
   } else {
     DefSpeed = 50;
   }
 
-  if (previousPulseTime != lastPulseTime) {
-    // Calculate frequency in Hz
-    frequency = 1000000 / (lastPulseTime - previousPulseTime);
-    previousPulseTime = lastPulseTime;
+  if(DefSpeedPrev != DefSpeed){
+    DefSpeedPrev = DefSpeed;
+    #ifdef OLED 
+      PrintStatus("Speed Changed");
+    #endif
+    Init_Time = millis();
   }
 
-  u8x8.setCursor(8,0);             // Column, Row
-  u8x8.print(DefSpeed);
-  u8x8.print("  ");
-  u8x8.setCursor(8,1);             // Column, Row
-  u8x8.print(DefAngle);
-  u8x8.print("  ");
-  u8x8.setCursor(8,2);             // Column, Row
-  u8x8.print(DefDelay);
-  u8x8.print("  ");
-  if (Steps == 490){
-    u8x8.setCursor(8,4);             // Column, Row
-    u8x8.print(frequency);
-    u8x8.print("  ");
-  }
-
-  if (Steps == 100){
+  if (((millis() - Init_Time) > 400) && Enable){
     if (RealSpeed != 0){
-      u8x8.setCursor(8,3);             // Column, Row
-      u8x8.print("Mantain");
-      u8x8.print(" ");
+      #ifdef OLED 
+        PrintStatus("Keep Speed");
+      #endif
       Enable = false;
     }
   }
 
-  if (Steps == 400){
+  if (((millis() - Init_Time) > 1400) && !Enable){
     if (RealSpeed != 0){
       configTimerForMeasure();
-      u8x8.setCursor(8,3);             // Column, Row
-      u8x8.print("Measure");
-      u8x8.print(" ");
+      // #ifdef OLED 
+      //   PrintStatus("Measuring");
+      // #endif
+      if (previousPulseTime != lastPulseTime) {
+        // Calculate frequency in Hz
+        frequency = 1000000 / (lastPulseTime - previousPulseTime);
+        previousPulseTime = lastPulseTime;
+      }
     }
   }
 
-  if (Steps == 500){
-    Steps = 0;
+  if ((millis() - Init_Time) > 2000){
+    Init_Time = 0;
     if (RealSpeed == 0) {
       configTimerForMeasure();
-      u8x8.setCursor(8,3);             // Column, Row
-      u8x8.print("Stopped");
-      u8x8.print(" ");
+      #ifdef OLED 
+        PrintStatus("Stopped");
+      #endif
     } else {
       configTimerForPulse();
+      #ifdef OLED 
+        PrintStatus("Energized");
+      #endif
       setWaveforms( RealSpeed , DefAngle );
-      u8x8.setCursor(8,3);             // Column, Row
-      u8x8.print("Running");
-      u8x8.print(" ");
       Enable = true;
     }
   }
-  
-  Steps++;
   
   if (!digitalRead(0)) {    // Accelerate
     if (DefSpeed > RealSpeed) {
@@ -176,24 +167,24 @@ void loop() {
     }
     if (NoPaso) {
       NoPaso = false;
-      u8x8.setCursor(8,3);             // Column, Row
-      u8x8.print("Accelera");
+      #ifdef OLED 
+        PrintStatus("Accelerating");
+      #endif
       configTimerForPulse();
       setWaveforms( 30 , DefAngle );
       Enable = true;
       accelerate(DefSpeed, DefDelay);
       setWaveforms( DefSpeed , DefAngle );
       RealSpeed = DefSpeed;
-      Steps = 0;
     }
     noBreake = true;
   }
 
   if (!digitalRead(1)) {    // Break
     if (noBreake) {
-      u8x8.setCursor(8,3);             // Column, Row
-      u8x8.print("Break");
-      u8x8.print("   ");
+      #ifdef OLED 
+        PrintStatus("Breaking");
+      #endif
       noBreake = false;
       while(digitalRead(3));  // Wait for Output 3 to go LOW
       breakMotor(RealSpeed, DefDelay);
@@ -201,9 +192,45 @@ void loop() {
       configTimerForMeasure();
       RealSpeed = 0;
       NoPaso = true;
+      Init_Time = millis();
+      Init_Time -= 2000;
     }
   }
+
+  
 }
+#ifdef OLED 
+void PrintStatus(String status){
+  // Write a menu on display
+  u8g2.clearBuffer();
+  u8g2.setCursor(0,10);             // Column, Row
+  u8g2.print("Speed : ");
+  u8g2.setCursor(0,20);             // Column, Row
+  u8g2.print("Angle : ");
+  u8g2.setCursor(0,30);             // Column, Row
+  u8g2.print("Delay : ");
+  u8g2.setCursor(0,40);             // Column, Row
+  u8g2.print("Status: ");
+  u8g2.setCursor(0,50);             // Column, Row
+  u8g2.print("Real F: ");
+  u8g2.setCursor(44,10);             // Column, Row
+  u8g2.print(DefSpeed);
+  u8g2.print("  ");
+  u8g2.setCursor(44,20);             // Column, Row
+  u8g2.print(DefAngle);
+  u8g2.print("  ");
+  u8g2.setCursor(44,30);             // Column, Row
+  u8g2.print(DefDelay);
+  u8g2.print("  ");
+  u8g2.setCursor(44,50);             // Column, Row
+  u8g2.print(frequency);
+  u8g2.print("  ");
+  u8g2.setCursor(44,40);             // Column, Row
+  u8g2.print(status);
+  u8g2.sendBuffer();
+}
+#endif
+
 
 void configTimerForMeasure(void){
   // Configure Timer 1 for Frequency Measurement
